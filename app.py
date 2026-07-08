534572cb8138ef809d1d0c2f729479d91518706f
darklauch <tobias.mielke.xbox@googlemail.com>
2026-07-08 09:38:38 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ python
 import streamlit as st
 import pandas as pd
 
-st.title("Mein Finanztool")
-st.write("Die App läuft! Jetzt verbinden wir als Nächstes deine DAten.")
+st.title("Finanz-Check")
+st.write("Die App läuft jetzt fehlerfrei!")
